c1e6f78886b0e74a5f83037aa0d2369dbbcdc945
filakm <filakm06@gmail.com>
2025-11-21 12:59:23 +0100

    21.11

---
+++ 07-Arrays/1-8.py
@@ -0,0 +1,14 @@
+computer_games = [
+   "Minecraft", "Fortnite", "Cyberpunk 2077", "The Witcher 3",
+   "League of Legends", "Valorant", "Grand Theft Auto V", 
+   "Elden Ring", "Apex Legends", "Call of Duty: Warzone"
+]
+num_of_games = 1
+while num_of_games <11 :
+   print(str(num_of_games)+". "+computer_games[num_of_games-1])
+   num_of_games += 1
+
+
+ 
+   
+    
---
+++ 07-Arrays/t.py
@@ -0,0 +1,5 @@
+arr = [2,3,7,5,4]
+
+for value in arr:
+    print(value)
+    
